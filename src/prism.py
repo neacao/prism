b04b0db7e6801032c31fb38d3db4e594219b353f
Nea <nea@vinova.sg>
2018-04-30 11:29:29 +0700

    DFS seq extension

--- src/prism.py
+++ src/prism.py
@@ -5,16 +5,35 @@ from constant import *
 from prism_encode import *
 from prism_extension import *
 
+def processSeqExtension(lastFrequent, items, primalSeqExt, primalPosSeqExt, primalSeqAllIteams, primalsPosAllItems):
+	lengthOfItems = len(primalSeqAllIteams)
+
+	for _index in xrange(0, lengthOfItems):
+		(_primalSeqExt, _primalPosSeqExt) = seqExtension(lastFrequent, items[_index], primalSeqExt, primalSeqAllIteams[_index], primalPosSeqExt, primalsPosAllItems[_index])
+		lastFrequent += "->{0}".format(items[_index])
+
+		if countingSupport(_primalSeqExt) >= MIN_SUPP:
+			if _index == 1:
+				exit(0)
+
+			print "##### Frequent seq: {0}\n".format(lastFrequent)
+			processSeqExtension(lastFrequent, items, _primalSeqExt, _primalPosSeqExt, primalSeqAllIteams, primalsPosAllItems)
+		
+		else:
+			print "######## Reject seq: {0}\n".format(lastFrequent)
+
+	return
+
+
 if __name__ == "__main__":
 	primalsPosAllItems = processPrimalEncodingPos()
 	primalSeqAllIteams = processPrimalEncodingSeq()
 
-	(primalSeqExt, primalPosSeqExt) = seqExtension(ITEMS[0], ITEMS[1], primalSeqAllIteams[0], primalSeqAllIteams[1], primalsPosAllItems[0], primalsPosAllItems[1])
-	seqExt2 = seqExtension("a->b", ITEMS[0], primalSeqExt, primalSeqAllIteams[0], primalPosSeqExt , primalsPosAllItems[0])
-
+	processSeqExtension(ITEMS[0], ITEMS, primalSeqAllIteams[0], primalsPosAllItems[0], primalSeqAllIteams, primalsPosAllItems)
 	
 
 
+	# Items extension only
 
 
 
