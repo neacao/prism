a20b4f0c404162043f295cb41cc7d3e68d168320
Nea Cao <neacao@gmail.com>
2019-05-01 17:37:50 +0700

    Implement encode siq & pos

--- src/New/PrismHelper.py
+++ src/New/PrismHelper.py
@@ -3,8 +3,12 @@
 import openpyxl, json
 
 class PrismHelper:
+	def __init__(self):
+		self.primeArray = [2, 3, 5, 7]
+		self.primeLength = len(self.primeArray)
+	# --
 
-	def convertHorizontalRecord(self, resourcePath):
+	def convertHorizontalRecord(self, resourcePath, outputPath):
 		wb = openpyxl.load_workbook(resourcePath)
 		ws = wb.active
 		wsRange = ws['A{}:I{}'.format(ws.min_row, ws.max_row)]
@@ -39,21 +43,123 @@ class PrismHelper:
 			else:
 				curString += (nextPosSyntax + encodeVal)
 			# -
+		# -
 
 		seqList = seqList[1:] # Remove first redundant element
 		seqList.append(curString) # Append last student info
 
-		for seq in seqList:
-			print('{}\n'.format(seq))
-		# -
+		fileOutputPath = '{}/CourseGradeEncodedHorizontal.json'.format(outputPath)
+		with open(fileOutputPath, 'w') as fp:
+			json.dump(seqList, fp, indent=2)
+		return fileOutputPath
 	# --
+
+
+	def createPrimalBlockEncode(self, horizontalRecordPath):
+		return 1
+	# --
+
+	# Encode primal blocks for single sequence
+	# sequence: a string contain course grade encoded
+	# item: numberic value
+	def createPrimalEncodedPos(self, item, sequence):
+		semesterList = sequence.split("->")
+		semesterLenght = len(semesterList)
+
+		counter = 0
+		ret = []
+
+		while counter < semesterLenght:
+			primalVal = 1
+			primseIdx = 0
+
+			for idx in range(0, self.primeLength): # Loop on blocks
+				realIdx = idx + counter
+				if realIdx == semesterLenght:
+					break
+
+				courseList = semesterList[realIdx].split(".")
+
+				for course in courseList:
+					if course == item:
+						primalVal *= self.primeArray[primseIdx]
+						break
+				# -
+				primseIdx += 1
+			# -
+			ret.append(primalVal)
+			primseIdx = 0
+			primalVal = 1
+			counter += self.primeLength
+		# -
+		return ret
+	# --
+
+	def createPrimalEncodedSiq(self, item, seqList):
+		counter = 0
+		seqListLength = len(seqList)
+		ret = []
+
+		while counter < seqListLength:
+			primalVal = 1
+			primeIdx = 0
+
+			endIdx = min(seqListLength - counter, self.primeLength)
+			subSeqList = seqList[counter: counter+endIdx]
+
+			for seq in subSeqList:
+				if self.doesItemInSequence(item, seq):
+					primalVal *= self.primeArray[primeIdx]
+				primeIdx += 1
+			# -
+			ret.append(primalVal)
+			counter += self.primeLength
+			primalVal = 1
+			primeIdx = 0
+		# -
+		return ret
+	# --
+
+
+	def doesItemInSequence(self, targetItem, sequence):
+		itemsetList = sequence.split('->')
+		for itemset in itemsetList:
+			if self.doesItemInItemset(targetItem, itemset):
+				return True
+		# -
+		return False
+	# --
+
+	def doesItemInItemset(self, targetItem, itemset):
+		itemList = itemset.split(".")
+		for item in itemList:
+			if targetItem == item:
+				return True
+		# -
+		return False
+	# --
+
 # --- PrismHelper
 
 
 if __name__ == "__main__":
 	litePath = "../../data/lite"
-	prismHelper = PrismHelper()
-	prismHelper.convertHorizontalRecord('{}/CourseGradeEncoded.xlsx'.format(litePath))
+	helper = PrismHelper()
+	helper.convertHorizontalRecord('{}/CourseGradeEncoded.xlsx'.format(litePath), litePath)
+
+	# Using block w/ length 4 to test -> [2, 3, 5, 7]
+	string = [
+		"a.b->b->b->a.b->b->a",
+		"a.b->b->b",
+		"b->a.b",
+		"b->b->b",
+		"a.b->a.b->a.b->a->b.c"
+	]
+
+	# for sub in string:
+	# 	helper.createPrimalEncodedPos('a', sub)
+	helper.createPrimalEncodedSiq('b', string)
+
 # ---
 
 
