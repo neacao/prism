c17fe4d8b73797e4d9659e1657fa20253219435e
Nea Cao <neacao@gmail.com>
2019-05-03 21:42:10 +0700

    Implement counting for joining operation

--- src/New/Prism.py
+++ src/New/Prism.py
@@ -13,17 +13,38 @@ class Prism:
 		self.rankGCDList = GCD
 	# --
 
+	def getRank(self, val):
+		try:
+			_index = self.rankList.index(val)
+			return _index
+		except ValueError:
+			print("[FATAL ERROR] Not found Rank of this: {0}".format(val))
+			exit(1)
+	# --
+
 	def getSupportOfList(self, primalList):
-		tempRankList = map(lambda primal: self.rankList.index(primal), primalList)
+		tempRankList = map(lambda primal: self.getRank(primal), primalList)
 		tempRankSupportList = map(lambda rank: self.rankSupportList[rank], tempRankList)
 		ret = reduce(lambda val, support: val + support, tempRankSupportList)
 		return ret
 	# --
 
 
+	def getGCDOfValues(self, val1, val2):
+		rankVal1 = self.getRank(val1)
+		rankVal2 = self.getRank(val2)
+		rankGCD = self.rankGCDList[rankVal1][rankVal2]
+		ret = self.rankList[rankGCD]
+		return ret
+	# --
+
 # ---
 
 if __name__ == "__main__":
 	prism = Prism(PrismHelper())
-	ret = prism.getSupportOfList([30, 210])
+	ret = prism.getGCDOfValues(35, 6)
 	print(ret)
+
+
+
+	
