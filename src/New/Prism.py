4f756ff0bf988d36511b268f56757005a2687252
Nea Cao <neacao@gmail.com>
2019-05-04 14:44:51 +0700

    Basic joining of a single sequence

--- src/New/Prism.py
+++ src/New/Prism.py
@@ -54,10 +54,10 @@ class Prism:
 		seqPrimal, offsets, posItems,
 		targetSeqPrimal, targetOffsets, targetPosItems):
 
-		print(posItems)
-		print(targetPosItems)
-
 		gcd = self.getGCD(seqPrimal, targetSeqPrimal)
+
+		print('seqPrimal {} - targetSeqPrimal {} - gcd {}'.format(seqPrimal, targetSeqPrimal, gcd))
+
 		primeIdx = 0
 		offsetIdx = 0
 		itemOffsetLength = len(offsets)
@@ -65,13 +65,33 @@ class Prism:
 
 		while gcd > 1:
 			primeVal = self.primeArray[primeIdx]
-
-			if gcd % primeVal: # Valid block to count pos blocks
+			print('> gcd: {}'.format(gcd))
+			if gcd % primeVal == 0: # Valid block to count pos blocks
 				# Get pos blocks to calculate
+				print('> Process at {}'.format(primeIdx))
+				startIndex = offsets[primeIdx].value
+				length = min(offsets[primeIdx].length, targetOffsets[primeIdx].length)
+				endIndex = startIndex + length
+				print('> startIndex {} - endIndex {}'.format(startIndex, endIndex))
 
+				for idx in range(startIndex, endIndex):
+					blockIdx = posItems[idx].blockIndex
+					targetIdx = targetPosItems[idx].blockIndex
+
+					# Make sure they same block index
+					if blockIdx == targetIdx:
+						posPrimal = posItems[idx].value
+						targetPrimal = targetPosItems[idx].value
+
+						_gcd = self.getGCD(posPrimal, targetPrimal)
+						print('  > pos blocks joining: posPrimal {} targetPrimal {} gcd {}'.format(posPrimal, targetPrimal, _gcd))
+					# -
+				# -
+				print('  < pos blocks joined')
 			# -
 
 			gcd /= self.primeArray[primeIdx]
+			primeIdx += 1
 		# -
 
 		return 1
@@ -86,11 +106,13 @@ class Prism:
 # ---
 
 if __name__ == "__main__":
-	helper = PrismHelper()
+	helper = PrismHelper(['a', 'b', 'c'])
 	prism = Prism(helper)
 	prismItems = list(helper.mockup())
 
-	prism.extendItemsetSingleBlock('a', prismItems, 'b', prismItems)
+	prism.extendItemsetSingleBlock(
+		prismItems[0].seqPrimals[0], prismItems[0].offsets[0], prismItems[0].posItems,
+		prismItems[1].seqPrimals[0], prismItems[1].offsets[0], prismItems[1].posItems)
 
 
 
